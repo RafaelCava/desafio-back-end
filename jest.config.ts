b41b66d6158f40d7798b422234a0bcdb955ff3cc
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-04-02 19:08:21 -0300

    chore: remove mongodb preset

--- jest.config.ts
+++ jest.config.ts
@@ -11,7 +11,6 @@ module.exports = {
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
-  preset: '@shelf/jest-mongodb',
   moduleNameMapper: {
     '@/tests/(.*)': '<rootDir>/tests/$1',
     '@/(.*)': '<rootDir>/src/$1'
