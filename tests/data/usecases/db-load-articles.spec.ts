8a21fb31b5370953332e4f99fa72563ac212379a
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-04-02 21:33:39 -0300

    feat: ensure DbLoadArticles returns an array with articles if then exists

--- tests/data/usecases/db-load-articles.spec.ts
+++ tests/data/usecases/db-load-articles.spec.ts
@@ -1,6 +1,8 @@
 import { type LoadArticlesRepository } from '@/data/protocols'
 import { DbLoadArticles } from '@/data/usecases'
 import { LoadArticlesRepositorySpy } from '@/tests/data/mocks/load-articles-repository'
+import { mockArticles } from '@/tests/domain/mocks'
+import Mockdate from 'mockdate'
 
 type SutTypes = {
   sut: DbLoadArticles
@@ -17,10 +19,24 @@ const makeSut = (): SutTypes => {
 }
 
 describe('DbLoadArticles', () => {
+  beforeEach(() => {
+    Mockdate.set(new Date())
+  })
+
+  afterEach(() => {
+    Mockdate.reset()
+  })
+
   it('should call LoadArticlesRepository once time', async () => {
     const { sut, loadArticlesRepositoryStub } = makeSut()
     const loadSpy = jest.spyOn(loadArticlesRepositoryStub, 'load')
     await sut.load()
     expect(loadSpy).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return an array with Articles if then exists', async () => {
+    const { sut } = makeSut()
+    const articles = await sut.load()
+    expect(articles).toEqual(mockArticles())
   })
 })
