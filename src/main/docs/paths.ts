6d8666a1dfc5d2bf9b9db824416ae660238ef0c9
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-04-05 15:15:04 -0300

    feat: add GET /articles/search/:term to swagger

--- src/main/docs/paths.ts
+++ src/main/docs/paths.ts
@@ -1,6 +1,11 @@
-import { loadArticlesPath, loadArticlesByCategoryPath } from './paths/'
+import {
+  loadArticlesPath,
+  loadArticlesByCategoryPath,
+  loadArticlesByTermPath,
+} from './paths/'
 
 export default {
   '/articles': loadArticlesPath,
   '/articles/{category}': loadArticlesByCategoryPath,
+  '/articles/search/{term}': loadArticlesByTermPath,
 }
--- src/main/docs/paths/index.ts
+++ src/main/docs/paths/index.ts
@@ -1,2 +1,3 @@
 export * from './load-articles-path'
 export * from './load-articles-by-category-path'
+export * from './load-articles-by-term-path'
