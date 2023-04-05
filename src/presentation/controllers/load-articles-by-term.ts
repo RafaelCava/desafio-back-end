f8aca697fa616ac07fad0423c9ab3eee08ff4bab
Rafael Cavalcante <rafael.cavalcante@tallos.com.br>
2023-04-05 12:52:54 -0300

    feat: ensure LoadArticlesByTermController return serverError if LoadArticlesByTerm throws

--- src/presentation/controllers/load-articles-by-term.ts
+++ src/presentation/controllers/load-articles-by-term.ts
@@ -1,13 +1,18 @@
 import { LoadArticlesByTerm } from '@/domain/usecases'
 import { Controller, HttpResponse } from '../protocols'
+import { serverError } from '../helpers/http-helper'
 
 export class LoadArticlesByTermController implements Controller {
   constructor(private readonly loadArticlesByTerm: LoadArticlesByTerm) {}
   async handle(
     request: LoadArticlesByTermController.Request
   ): Promise<LoadArticlesByTermController.Result> {
-    await this.loadArticlesByTerm.loadByTerm(request.term)
-    return null as any
+    try {
+      await this.loadArticlesByTerm.loadByTerm(request.term)
+      return null as any
+    } catch (error) {
+      return serverError(error)
+    }
   }
 }
 
--- tests/presentation/controllers/load-articles-by-term.spec.ts
+++ tests/presentation/controllers/load-articles-by-term.spec.ts
@@ -1,6 +1,8 @@
 import { LoadArticlesByTerm } from '@/domain/usecases'
+import { serverError } from '@/presentation/helpers/http-helper'
 import { LoadArticlesByTermController } from '@/presentation/controllers'
 import { LoadArticlesByTermSpy } from '../mocks/load-articles'
+import { throwError } from '@/tests/domain/mocks'
 
 type SutTypes = {
   sut: LoadArticlesByTermController
@@ -27,5 +29,14 @@ describe('LoadArticlesByTerm Controller', () => {
     await sut.handle(mockRequest())
     expect(loadByTermSpy).toHaveBeenCalledWith(mockRequest().term)
     expect(loadByTermSpy).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return ServerError if LoadArticlesByTerm throws', async () => {
+    const { sut, loadArticlesByTermSpy } = makeSut()
+    jest
+      .spyOn(loadArticlesByTermSpy, 'loadByTerm')
+      .mockImplementationOnce(throwError)
+    const articles = await sut.handle(mockRequest())
+    expect(articles).toEqual(serverError(new Error().stack as string))
   })
 })
